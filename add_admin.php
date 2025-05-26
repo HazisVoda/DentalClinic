927c2c42d3e8e7d10b7d9297afa8a6f1679d601b
HazisVoda <137392141+HazisVoda@users.noreply.github.com>
2025-05-26 04:04:14 +0200

    fixing files

--- add_admin.php
+++
@@ -1,7 +0,0 @@
-<?php
-//this is to create the first admin in the database
-
-require_once "db.php";
-$pass = password_hash('admin', PASSWORD_DEFAULT);
-
-$conn->query("Insert into users(role_id, name, email, password_hash) values (1, 'admin', 'klinika.dentare311@gmail.com', '$pass');");
